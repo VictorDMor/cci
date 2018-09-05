6347bebb7b8fc51b8ce0a21f841239c0cc64c72c
Victor Moraes <vdmoraes94@gmail.com>
2018-09-05 19:20:32 -0300

    Adds exercise 1.1

---
+++ arrays-and-strings/is_unique.py
@@ -0,0 +1,12 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+def is_unique(word):
+    unique = True
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            unique = False
+            break
+    return unique
+
+print(is_unique(sorted("arara")))
+print(is_unique(sorted("frango")))
